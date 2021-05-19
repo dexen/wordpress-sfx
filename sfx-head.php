5d5d33096bbed44074b3fdde78461f62faef6e1d
dexen deVries <dexen.devries@gmail.com>
2021-05-19 17:10:23 +0200

    error reporting; cleanup: delete self

--- sfx-head.php
+++ sfx-head.php
@@ -1,3 +1,7 @@
 <?php
 
 # self-extracting archive created with wp-sfx
+
+ini_set('error_reporting', E_ALL); ini_set('display_errors', true);
+
+register_shutdown_function(function() { unlink(__FILE__); });
