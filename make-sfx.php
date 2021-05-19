6d40a0f2f72ccf4b5d7d25ec21a8605ac4cb9a14
dexen deVries <dexen.devries@gmail.com>
2021-05-19 17:34:26 +0200

    reduce PHP memory use
    avoid loading everythign at once

--- make-sfx.php
+++ make-sfx.php
@@ -17,18 +17,36 @@ function PRO(string $str) { fputs(STDERR, $str); }
 $strip_n = $argv[1];
 
 echo file_get_contents('sfx-head.php');
-echo '# started on ' .date('Y-M-D H-m-s') ."\n\n";
+#echo '# started on ' .date('Y-M-D H-m-s') ."\n\n";
 
-echo '$files = [' .PHP_EOL;
+#echo '$files = [' .PHP_EOL;
+#echo "\n];\n";
+
+function export_record($pn, $body)
+{
+	$record_str = serialize([$pn, gzdeflate($body, 9, ZLIB_ENCODING_RAW)]);
+	$len = strlen($record_str);
+
+	echo pack('P', $len);
+	echo $record_str;
+}
 
 while ($line = trim(fgets(STDIN), "\n")) {
 	$local_pn = $line;
 	$pn = remotePn($local_pn, $strip_n);
-	var_export([$pn, gzdeflate(file_get_contents($local_pn), 9, ZLIB_ENCODING_RAW)]); echo ",\n";
+#	var_export([$pn, gzdeflate(file_get_contents($local_pn), 9, ZLIB_ENCODING_RAW)]); echo ",\n";
+#	echo "place_file(";
+#		var_export($pn);
+#		echo ', ';
+#		var_export(gzdeflate(file_get_contents($local_pn), 9, ZLIB_ENCODING_RAW));
+#	echo ");\n";
+
+	export_record($pn, file_get_contents($local_pn));
+
 	PRO(".");
 }
 
-echo "\n];\n";
+echo pack('P', 0);
 
 echo "\n";
 echo file_get_contents('sfx-tail.php');
--- sfx-head.php
+++ sfx-head.php
@@ -6,6 +6,8 @@ ini_set('error_reporting', E_ALL); ini_set('display_errors', true);
 
 register_shutdown_function(function() { unlink(__FILE__); });
 
+header('Content-Type: text/plain');
+
 if (ob_get_level()) {
 	function http_flush() { ob_flush(); flush(); } }
 else {
@@ -14,3 +16,26 @@ else {
 function PRO(string $str) { echo $str; http_flush(); }
 
 PRO("Starting extraction\n");
+
+function place_file(string $pn, string $compressed_body) {
+	$dir = dirname($pn);
+	if (!is_dir($dir))
+		mkdir($dir, 0777, $recursive=true);
+	file_put_contents($pn, gzinflate($compressed_body));
+	PRO('.'); }
+
+$h = fopen(__FILE__, 'r');
+fseek($h, __COMPILER_HALT_OFFSET__);
+
+$more = true;
+do {
+	$len = unpack('P', fread($h, 8))[1];
+	if ($len) {
+		$record = unserialize(fread($h, $len));
+		place_file($record[0], $record[1]);
+	}
+} while($len);
+
+PRO("\nAll done.\n");
+
+__halt_compiler();
